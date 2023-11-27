a9398e7dbeb5ce1527916bf6d0c61dabb1e9ec62
github_darrellv14 <darrell.valentino14@gmail.com>
2023-11-27 15:55:01 +0700

    Pertemuan 27 November 2023

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -11,16 +11,33 @@
     <form action="/pegawai/store" method="post" class="form-horizontal" role="form">
         {{ csrf_field() }}
         <div class = "form-group">
-            <label for = "nama" class = "col-sm-2 control-label">First Name</label>
+            <label for = "nama" class = "col-sm-2 control-label">Nama</label>
             <div class = "col-sm-10">
                 <input type = "text" class = "form-control" id = "nama" placeholder = "Enter First Name">
             </div>
         </div>
-        Nama <input type="text" name="nama" required="required"> <br />
-        Jabatan <input type="text" name="jabatan" required="required"> <br />
+        <div class = "form-group">
+            <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+            <div class = "col-sm-10">
+                <input type = "text" class = "form-control" id = "jabatan" placeholder = "Enter First Name">
+            </div>
+        </div>
+        <div class = "form-group">
+            <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+            <div class = "col-sm-10">
+                <input type = "number" class = "form-control" id = "umur" placeholder = "Enter First Name">
+            </div>
+        </div>
+        <div class = "form-group">
+            <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+            <div class = "col-sm-10">
+                <text type = "text" class = "form-control" id = "alamat" placeholder = "Enter First Name">
+            </div>
+        </div>
+        {{-- Jabatan <input type="text" name="jabatan" required="required"> <br />
         Umur <input type="number" name="umur" required="required"> <br />
         Alamat
-        <textarea name="alamat" required="required"></textarea> <br />
-        <input type="submit" value="Simpan Data">
+        <textarea name="alamat" required="required"></textarea> <br /> --}}
+        <input type="submit" value="Simpan Data" class="btn btn-primary">
     </form>
 @endsection
